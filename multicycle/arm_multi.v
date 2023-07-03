2609721cb9c549c07a9e5916875f6866ce0123c0
anaaccilio2004 <ana.accilio@utec.edu.pe>
2023-07-03 13:24:58 -0500

    alu_mull_new

--- multicycle/arm_multi.v
+++ multicycle/arm_multi.v
@@ -945,8 +945,8 @@ module alu(a,b,ALUControl,Result,ALUFlags);
 		  R_mull = 64'b0;
 		  neg = Result[31];
           zero = (Result == 32'b0);
-          carry = (ALUControl[1] == 1'b0) & sum[32];
-          overflow = (ALUControl[1] == 1'b0) & ~(a[31] ^ b[31] ^ ALUControl[0]) & (a[31] ^ sum[31]);
+		  carry = (ALUControl[1] == 1'b0) & sum[32];
+		  overflow = (ALUControl[1] == 1'b0) & ~(a[31] ^ b[31] ^ ALUControl[0]) & (a[31] ^ sum[31]);
 
 		end
         endcase
